7f93fb6991e5f20f54cecc5fcb819cd18e0c25e8
Toby Hsieh <thrasher13b@gmail.com>
2019-12-15 16:05:18 -0800

    Update MainActivityTest to not use deprecated Robolectric techniques

--- app/src/test/java/io/github/tobyhs/screenlighter/MainActivityTest.java
+++ app/src/test/java/io/github/tobyhs/screenlighter/MainActivityTest.java
@@ -5,18 +5,21 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.provider.Settings;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowApplication;
 import org.robolectric.shadows.ShadowToast;
 
 import io.github.tobyhs.screenlighter.shadows.ShadowSettingsSystem;
+
+import static androidx.test.core.app.ActivityScenario.launch;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -31,30 +34,36 @@ public class MainActivityTest {
     public void increaseBrightness() {
         ContentResolver contentResolver = application.getContentResolver();
         Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, 5);
-        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
 
-        int brightness = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, 0);
-        assertThat(brightness, is(15));
+        try (ActivityScenario<MainActivity> scenario = launch(MainActivity.class)) {
+            int brightness = Settings.System.getInt(
+                    contentResolver, Settings.System.SCREEN_BRIGHTNESS, 0
+            );
+            assertThat(brightness, is(15));
 
-        assertThat(ShadowToast.shownToastCount(), is(1));
-        String actualToastText = ShadowToast.getTextOfLatestToast();
-        String expectedToastText = application.getResources().getString(R.string.increased_screen_brightness);
-        assertThat(actualToastText, is(expectedToastText));
+            assertThat(ShadowToast.shownToastCount(), is(1));
+            String actualToastText = ShadowToast.getTextOfLatestToast();
+            String expectedToastText = application.getResources().getString(
+                    R.string.increased_screen_brightness
+            );
+            assertThat(actualToastText, is(expectedToastText));
 
-        assertThat(activity.isFinishing(), is(true));
+            assertThat(scenario.getState(), is(Lifecycle.State.DESTROYED));
+        }
     }
 
     @Test
     @Config(shadows = {ShadowSettingsSystem.class})
     public void requestWriteSettingsPermission() {
         ShadowSettingsSystem.setCanWrite(false);
-        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
 
-        ShadowApplication shadowApp = shadowOf(application);
-        Intent activityIntent = shadowApp.getNextStartedActivity();
-        assertThat(activityIntent.getAction(), is(Settings.ACTION_MANAGE_WRITE_SETTINGS));
-        assertThat(activityIntent.getDataString(), is("package:io.github.tobyhs.screenlighter"));
+        try (ActivityScenario<MainActivity> scenario = launch(MainActivity.class)) {
+            ShadowApplication shadowApp = shadowOf(application);
+            Intent activityIntent = shadowApp.getNextStartedActivity();
+            assertThat(activityIntent.getAction(), is(Settings.ACTION_MANAGE_WRITE_SETTINGS));
+            assertThat(activityIntent.getDataString(), is("package:io.github.tobyhs.screenlighter"));
 
-        assertThat(activity.isFinishing(), is(true));
+            assertThat(scenario.getState(), is(Lifecycle.State.DESTROYED));
+        }
     }
 }
